ea1e0f742345138d61457f543050c3780caea5ad
HolographicWings <ewandenielvalero@orange.fr>
2022-08-06 19:05:10 +0200

    Update image-previewer.js
    replace spaces by tabulations

--- image-previewer.js
+++ image-previewer.js
@@ -1,59 +1,59 @@
 class ImagePreviewer extends Application {
-    
-    static get defaultOptions() {
-        this.imageUrl = '';
-        const options = super.defaultOptions;
-        options.template = "modules/image-previewer/template.html";
-        options.width = 200;
-        options.height = 200;
-        options.classes = ['image-previewer'];
-        return options;
-    }
 
-    getData() {
-        return { preview: this.imageUrl}
-    }
+	static get defaultOptions() {
+		this.imageUrl = '';
+		const options = super.defaultOptions;
+		options.template = "modules/image-previewer/template.html";
+		options.width = 200;
+		options.height = 200;
+		options.classes = ['image-previewer'];
+		return options;
+	}
 
-    showPreview(imageUrl, previewPos) {
-        this.imageUrl = imageUrl;
-        this._render(true);
-        this.position.top = previewPos.y;
-        this.position.left = previewPos.x;
-        // this.shortTimeout;  used to close preview when unhover
-        // this.longTimeout;   used to close preview after 2 seconds
+	getData() {
+		return { preview: this.imageUrl}
+	}
 
-        // prevent shortTimeout to close the preview since another previewable item was hovered
-        if (this.shortTimeout !== undefined) clearTimeout(this.shortTimeout)
+	showPreview(imageUrl, previewPos) {
+		this.imageUrl = imageUrl;
+		this._render(true);
+		this.position.top = previewPos.y;
+		this.position.left = previewPos.x;
+		// this.shortTimeout;  used to close preview when unhover
+		// this.longTimeout;   used to close preview after 2 seconds
 
-        // start a new longTimeout to close the preview after too long idle time
-        if (this.longTimeout !== undefined)  clearTimeout(this.longTimeout);
-        this.longTimeout = window.setTimeout(() => { this.close(); }, 2000);
-    }
+		// prevent shortTimeout to close the preview since another previewable item was hovered
+		if (this.shortTimeout !== undefined) clearTimeout(this.shortTimeout)
 
-    hoverOff() {
-        // closing the current preview after a short grace period
-        this.shortTimeout = window.setTimeout(() => {
-            this.close();
-        }, 100);
-    }
+		// start a new longTimeout to close the preview after too long idle time
+		if (this.longTimeout !== undefined)  clearTimeout(this.longTimeout);
+		this.longTimeout = window.setTimeout(() => { this.close(); }, 2000);
+	}
+
+	hoverOff() {
+		// closing the current preview after a short grace period
+		this.shortTimeout = window.setTimeout(() => {
+			this.close();
+		}, 100);
+	}
 }
 
 Hooks.on('renderFilePicker', (app, html, data) => {
-    let imagePreviewer = new ImagePreviewer();
-    html.find('.file').hover(ev => {
-        // get new position for the previewer
-        let elementBox = ev.target.getBoundingClientRect()
-        let previewPos = {
-            x: elementBox.x + elementBox.width,
-            y: elementBox.y
-        }
-        // get the proper image path
-        let path = ev.target.dataset.path;
-        let fileExtension = path.split('.')[path.split('.').length - 1].toLowerCase();
-        if (fileExtension in CONST.IMAGE_FILE_EXTENSIONS) {
-            imagePreviewer.showPreview(path, previewPos);
-        }
-    }, ev => {
-        imagePreviewer.hoverOff();
-    });
+	let imagePreviewer = new ImagePreviewer();
+	html.find('.file').hover(ev => {
+		// get new position for the previewer
+		let elementBox = ev.target.getBoundingClientRect()
+		let previewPos = {
+			x: elementBox.x + elementBox.width,
+			y: elementBox.y
+		}
+		// get the proper image path
+		let path = ev.target.dataset.path;
+		let fileExtension = path.split('.')[path.split('.').length - 1].toLowerCase();
+		if (fileExtension in CONST.IMAGE_FILE_EXTENSIONS) {
+			imagePreviewer.showPreview(path, previewPos);
+		}
+	}, ev => {
+		imagePreviewer.hoverOff();
+	});
 });
